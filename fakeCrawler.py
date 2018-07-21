68917a9049caad5815f86f2f0b3b08d1b6d04112
anberns <anberns@gmail.com>
2018-07-21 15:20:29 -0400

    heroku url

--- fakeCrawler.py
+++ fakeCrawler.py
@@ -20,7 +20,7 @@ def crawl():
 			await websocket.send(now)
 			await asyncio.sleep(random.random() * 3)
 
-	start_server = websockets.serve(time, '127.0.0.1', 8002)
+	start_server = websockets.serve(time, 'https://mysterious-springs-48423.herokuapp.com/', 8002)
 
 	asyncio.get_event_loop().run_until_complete(start_server)
 	asyncio.get_event_loop().run_forever()
